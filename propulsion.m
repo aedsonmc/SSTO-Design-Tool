cd0d0eb2a096898136bc7a8f5c3d7e5f64e65342
Alex Edson <aedsonmc@gmail.com>
2018-01-24 23:09:37 -0700

    Added Stuff

--- propulsion.m
+++ propulsion.m
@@ -23,3 +23,54 @@
 %
 
 %% Main
+%LOX Variables
+OF_LOX = 5;
+gamma_LOX = 1.25; %At nozzle exit
+a_LOX = 1122.9; %m/s speed of sound
+Me_LOX = 3.563; %Mach Number at exit
+Ve_LOX = Me_LOX * a_LOX;
+
+%Air Variables
+OF_Air = 33;
+gamma_Air = 1.32; %at nozzle exit
+a_Air = 605.2; %m/s speed of sound
+Me_Air = 3.696; %Mach Number at exit
+Ve_Air = Me_Air * a_Air;
+
+%Thrust Range Input
+T = linspace(8.5e5,2e6,1000);
+
+%Calculate Reqired Exit Mass Flow
+m_dot_Exit_LOX = T./Ve_LOX;
+m_dot_Exit_Air = T./Ve_Air;
+
+%Calculate Oxidizer and Fuel Mass Flow Rates
+m_dot_F_LOX = m_dot_Exit_LOX./OF_LOX;
+m_dot_F_Air = m_dot_Exit_Air./OF_Air;
+m_dot_Ox_LOX = m_dot_Exit_LOX - m_dot_F_LOX;
+m_dot_Ox_Air = m_dot_Exit_Air - m_dot_F_Air;
+
+%Plot Required Flow Rates
+figure(1)
+plot(T,m_dot_Exit_LOX,T,m_dot_Exit_Air)
+title('Exit Mass Flow')
+xlabel('Thrust')
+ylabel('Mass Flow Rate (kg/s)')
+grid on
+legend('LOX','Air','location','NorthWest')
+
+figure(2)
+plot(T,m_dot_Ox_LOX,T,m_dot_Ox_Air)
+title('Oxidizer Mass Flow')
+xlabel('Thrust')
+ylabel('Mass Flow Rate (kg/s)')
+grid on
+legend('LOX','Air','location','NorthWest')
+
+figure(3)
+plot(T,m_dot_F_LOX,T,m_dot_F_Air)
+title('Fuel Mass Flow')
+xlabel('Thrust')
+ylabel('Mass Flow Rate (kg/s)')
+grid on
+legend('LOX','Air','location','NorthWest')
